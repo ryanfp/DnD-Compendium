2eb1f92f21069d7f6529d37c3bc83c235ff10447
ryanfp <71905151+ryanfp@users.noreply.github.com>
2025-07-06 23:50:45 -0400

    DnD Vault 2025-07-06 23:50
    Affected files:
    .obsidian/workspace.json
    publish.css
    publish.js
    publish1.css

--- publish.js
+++ publish.js
@@ -1,5 +1,63 @@
-document.body.addClass('dnd');
 
+
+// Initialize function for the created and updated properties
+let id;
+
+function insertMetaDates() {
+  const frontmatter = app.site.cache.cache[app.currentFilepath].frontmatter;
+  if (!frontmatter) {
+    return;
+  }
+
+  const created = frontmatter["created"]?.replaceAll("-", "/");
+  const updated = frontmatter["updated"]?.replaceAll("-", "/");
+  if (!created && !updated) {
+    return;
+  }
+
+  const frontmatterEl = document.querySelector(".frontmatter");
+  if (!frontmatterEl) {
+    return;
+  }
+
+  frontmatterEl.insertAdjacentHTML(
+    "afterend",
+    `
+<div style="display: flex; justify-content: end; gap: 3px;">
+    <a href="#${created}" class="tag" target="_blank" rel="noopener">
+        Created:${created}
+    </a>
+    <a href="#${updated}" class="tag" target="_blank" rel="noopener">
+        Updated:${updated}
+    </a>
+</div>
+`
+  );
+
+  clearInterval(id);
+}
+
+const onChangeDOM = (mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (
+      mutation.type === "childList" &&
+      mutation.addedNodes[0]?.className === "page-header"
+    ) {
+      clearInterval(id);
+      id = setInterval(insertMetaDates, 50);
+    }
+  }
+};
+
+const targetNode = document.querySelector(
+  ".markdown-preview-sizer.markdown-preview-section"
+);
+const observer = new MutationObserver(onChangeDOM);
+observer.observe(targetNode, { childList: true, subtree: true });
+id = setInterval(insertMetaDates, 50);
+
+
+// initialize funciton for the tag properties
 let id;
 
 function insertMetaDates() {
@@ -25,6 +83,8 @@ const observer = new MutationObserver(onChangeDOM);
 observer.observe(targetNode, { childList: true, subtree: true });
 id = setInterval(insertMetaDates, 50);
 
+
+// secondary function for created and updated properties
 function insertMetaDates() {
   const frontmatter = app.site.cache.cache[app.currentFilepath].frontmatter;
   if (!frontmatter) {
@@ -57,6 +117,8 @@ function insertMetaDates() {
   clearInterval(id);
 }
 
+
+// secondary function for the tags properties
 function insertMetaDates() {
   const frontmatter = app.site.cache.cache[app.currentFilepath].frontmatter;
   if (!frontmatter) {
@@ -93,3 +155,7 @@ function insertMetaDates() {
 
   clearInterval(id);
 }
+
+
+// Switches the custom theme to DND
+document.body.addClass('dnd');
