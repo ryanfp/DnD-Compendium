2d2972e4bc3bea3c1150801c6550184601325ff2
ryanfp <71905151+ryanfp@users.noreply.github.com>
2025-07-12 18:39:09 -0400

    DnD Vault 2025-07-12 18:39
    Affected files:
    .obsidian/backlink.json
    .obsidian/plugins/obsidian-linter/data.json
    publish.css
    publish.js

--- publish.js
+++ publish.js
@@ -5,6 +5,9 @@ console.log("Load start publish.js");
 document.querySelector("head > link[rel=icon]").href =
   "https://publish-01.obsidian.md/access/35d05cd1bf5cc500e11cc8ba57daaf88/favicon.ico";
 
+
+  /* Show Date and Time for Creation/Updating */
+  
 let id;
 
 function insertMetaData() {
@@ -25,7 +28,7 @@ function insertMetaData() {
 
   const frontmatterEl = document.querySelector(".frontmatter");
   if (!frontmatterEl) {
-    // DOM Don't use clearInterval because it is likely that the prepartaion is not yet complete
+    // DOM Don't use clearInterval because it is likely that the preparation is not yet complete
     return;
   }
 
@@ -54,7 +57,7 @@ const onChangeDOM = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
     if (
       mutation.type === "childList" &&
-      mutation.addedNodes[0]?.className === "page-header"
+      mutation.addedNodes[0]?.className === "publish-article-heading"
     ) {
       clearInterval(id);
       id = setInterval(insertMetaData, 50);
@@ -69,78 +72,11 @@ const observer = new MutationObserver(onChangeDOM);
 observer.observe(targetNode, { childList: true, subtree: true });
 id = setInterval(insertMetaData, 50);
 
+
+
 // Switches the custom theme to DND
 
 document.body.addClass('dnd');
-
-/* Frontmatter Initialize Block
-let id;
-
-function insertMetaDates() {
-  // TODO: 
-}
-
-const onChangeDOM = (mutationsList, observer) => {
-  for (let mutation of mutationsList) {
-    if (
-      mutation.type === "childList" &&
-      mutation.addedNodes[0]?.className === "page-header"
-    ) {
-      clearInterval(id);
-      id = setInterval(insertMetaDates, 50);
-    }
-  }
-};
-
-const targetNode = document.querySelector(
-  ".markdown-preview-sizer.markdown-preview-section"
-);
-const observer = new MutationObserver(onChangeDOM);
-observer.observe(targetNode, { childList: true, subtree: true });
-id = setInterval(insertMetaDates, 50);
-
-
-/* Use Frontmatter Function to Display DatesTimes 
-function insertMetaDates(...args: []) {
-    const frontmatter = app.site.cache.cache[app.currentFilepath].frontmatter;
-    if (!frontmatter) {
-        clearInterval(id);
-        return;
-    }
-
-    const created = frontmatter["created"]?.replaceAll("-", "/");
-    const updated = frontmatter["updated"]?.replaceAll("-", "/");
-    if (!created && !updated) {
-        clearInterval(id);
-        return;
-    }
-
-    const frontmatterEl = document.querySelector(".frontmatter");
-    if (!frontmatterEl) {
-        return;
-    }
-
-    frontmatterEl.insertAdjacentHTML(
-        "afterend",
-        `
-<div class="properties">
-    <div class="created">created:${created}</div>
-    <div class="updated">updated:${updated}</div>
-</div>
-`
-    );
-
-    clearInterval(id);
-}
-
-*/
-
-
-
-
-
-
-
 
 
 
@@ -186,7 +122,7 @@ const onChangeDOM = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
     if (
       mutation.type === "childList" &&
-      mutation.addedNodes[0]?.className === "page-header"
+      mutation.addedNodes[0]?.className === "publish-article-heading"
     ) {
       clearInterval(id);
       id = setInterval(insertMetaDates, 50);
